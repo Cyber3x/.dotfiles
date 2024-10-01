f56424c91f3eb38a27493e7393b1b7e5153d3ec8
Neven Lukic <neven.lukic8@gmail.com>
2024-10-02 00:42:24 +0200

    22 current  2024-10-02 00:42:18  24.05.5366.fbca5e745367  6.6.52                          *

--- nixos/common.nix
+++ nixos/common.nix
@@ -18,6 +18,11 @@
   # NixOS
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  nix.extraOptions = ''
+    extra-substituters = https://nixpkgs-python.cachix.org https://devenv.cachix.org;
+    extra-trusted-public-keys = nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=;
+  '';
+
   # Enable bluetooth
   hardware.bluetooth.enable = true;
 
