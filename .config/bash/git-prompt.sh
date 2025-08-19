cf6895fc0ccc83fca442ea617edb5a397ddf9727
cyber3x <neven.lukic8@gmail.com>
2025-08-19 12:43:52 +0200

    refactor into multiple small files

--- .config/bash/git-prompt.sh
+++ .config/bash/git-prompt.sh
@@ -1,6 +1,4 @@
 #https://bash-prompt-generator.org/
-PROMPT_COMMAND='PS1_CMD1=$(__git_ps1 "[%s]")'; PS1='\[\e[2m\][\[\e[38;5;208;1m\]\u\[\e[39m\]@\h\[\e[0;2m\]]\[\e[0m\] \[\e[2m\][\[\e[38;5;110;1m\]\w\[\e[0;2m\]]\[\e[0m\] \[\e[1m\]${PS1_CMD1}\n\[\e[0;2m\]\$\[\e[0m\] '
-
 export GIT_PS1_SHOWDIRTYSTATE=1        # Show staged (+) / unstaged (*) changes
 export GIT_PS1_SHOWUNTRACKEDFILES=1    # Show untracked files (%)
 export GIT_PS1_SHOWSTASHSTATE=1        # Show stash ($)
