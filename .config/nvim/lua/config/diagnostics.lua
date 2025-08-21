aa4acfe8d36dcbb0e5030ccfe5a469d959ab3437
cyber3x <neven.lukic8@gmail.com>
2025-08-21 23:30:49 +0200

    simplified diagnostics and diagnostics messages

--- .config/nvim/lua/config/diagnostics.lua
+++ .config/nvim/lua/config/diagnostics.lua
@@ -1,21 +1,13 @@
 -- Diagnostic configuration
 vim.diagnostic.config {
-  severity_sort = true,
-  float = { border = 'rounded', source = 'if_many' },
-  virtual_text = {
-    current_line = true,
-    source = 'if_many',
-    spacing = 2,
-    format = function(diagnostic)
-      return diagnostic.message
-    end,
-  },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = '󰅚',  -- Error icon
-      [vim.diagnostic.severity.WARN] = '󰀪',   -- Warning icon
-      [vim.diagnostic.severity.INFO] = '󰋽',   -- Info icon
-      [vim.diagnostic.severity.HINT] = '󰌶',   -- Hint icon
+    severity_sort = true,
+    virtual_text = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚', -- Error icon
+            [vim.diagnostic.severity.WARN] = '󰀪', -- Warning icon
+            [vim.diagnostic.severity.INFO] = '󰋽', -- Info icon
+            [vim.diagnostic.severity.HINT] = '󰌶', -- Hint icon
+        },
     },
-  },
 }
