0a61072913798a0e445091256041b5690a7c0bf3
nlukic <neven.lukic8@gmail.com>
2025-08-22 14:59:57 +0200

    nice formating for completon

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,7 +9,10 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
+vim.o.pumheight = 12
+
 local opt = vim.opt
+
 
 opt.number = true
 opt.relativenumber = true
@@ -22,21 +25,21 @@ opt.smartindent = true
 
 opt.wrap = false
 
-opt.swapfile = false  -- Disables swap files
-opt.backup = false    -- Disables backup files
+opt.swapfile = false -- Disables swap files
+opt.backup = false   -- Disables backup files
 
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 
-opt.hlsearch = false   -- Disable highlighting of search results
-opt.incsearch = true   -- Enable incremental search (shows results as you type)
+opt.hlsearch = false -- Disable highlighting of search results
+opt.incsearch = true -- Enable incremental search (shows results as you type)
 opt.ignorecase = true
 opt.smartcase = true
 
 opt.termguicolors = true
 
-opt.scrolloff = 8       -- Keeps at least 8 lines above/below the cursor
-opt.signcolumn = "yes"  -- Always show the sign column (prevents UI shifting)
+opt.scrolloff = 8         -- Keeps at least 8 lines above/below the cursor
+opt.signcolumn = "yes"    -- Always show the sign column (prevents UI shifting)
 opt.isfname:append("@-@") -- Allows "@" in filenames
 
 opt.updatetime = 50
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -9,14 +9,44 @@ return {
     },
     config = function()
         local cmp = require("cmp")
-        
+
         cmp.setup({
+            formatting = {
+                format = function(entry, vim_item)
+                    local max_width = 40
+                    if #vim_item.abbr > max_width then
+                        vim_item.abbr = string.sub(vim_item.abbr, 1, max_width) .. "..."
+                    end
+                    return vim_item
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered({
+                    border = "single",
+                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+                    col_offset = 0,
+                    side_padding = 0,
+                }),
+                documentation = cmp.config.window.bordered({
+                    border = "single",
+                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+                }),
+            },
+            experimental = {
+                ghost_text = true,
+            },
             mapping = cmp.mapping.preset.insert({
                 ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
+                ["<C-e>"] = cmp.mapping.abort(), -- close menu
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+                -- suggestion navigation
                 ["<Tab>"] = cmp.mapping.select_next_item(),
                 ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+
+                -- docs
+                ["<A-j>"] = cmp.mapping.scroll_docs(4),  -- scroll down in docs
+                ["<A-k>"] = cmp.mapping.scroll_docs(-4), -- scroll up in docs
             }),
             sources = {
                 { name = "nvim_lsp" },
@@ -26,4 +56,3 @@ return {
         })
     end,
 }
-
