d845fe759253d89067971ada5829250019ac5a61
cyber3x <neven.lukic8@gmail.com>
2025-08-28 12:40:09 +0200

    changes to the color, trying to get catpuchin to work and show all color

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,8 +1,25 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-        vim.cmd([[colorscheme tokyonight]])
-    end,
+    -- {
+    --     -- enabled = false,
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    --     priority = 1000, -- make sure to load this before all the other start plugins
+    --     config = function()
+    --         vim.cmd([[colorscheme tokyonight]])
+    --     end,
+    -- },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            -- # TODO : figure out if this even works, since it looks
+            -- way differnet than in the pictures
+            vim.cmd([[colorscheme catppuccin-macchiato]])
+
+            require("catppuccin").setup({
+                auto_integrations = true,
+            })
+        end,
+    }
 }
