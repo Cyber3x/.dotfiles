8571463aad1fb1c0567c9edca91653806eee75fa
cyber3x <neven.lukic8@gmail.com>
2025-09-03 15:32:58 +0200

    change man pager to be bat to get color

--- .config/bash/bash_exports.sh
+++ .config/bash/bash_exports.sh
@@ -10,6 +10,8 @@ shopt -s checkwinsize          # Update window size after each command
 
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
+export MANPAGER="sh -c 'col -bx | bat -l man'"
+
 PATH_DIRS=(
     /usr/local/go/bin
     /go/bin
