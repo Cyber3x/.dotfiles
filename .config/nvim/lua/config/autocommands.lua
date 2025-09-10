a3f2637366383edfc44820fd211d57625f8b1771
cyber3x <neven.lukic8@gmail.com>
2025-09-10 13:35:46 +0200

    add mini indent line and nvim now reads changes from disc

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -8,6 +8,11 @@ function M.setup()
 			vim.cmd("startinsert")
 		end,
 	})
+
+	vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+		command = "if mode() != 'c' | checktime | endif",
+		pattern = { "*" },
+	})
 end
 
 return M
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,3 +1,15 @@
 return {
-    { 'echasnovski/mini.comment', version = false }
+	{ "echasnovski/mini.comment", version = false, opts = {} },
+	{
+		"nvim-mini/mini.indentscope",
+		version = false,
+		opts = function()
+			local indentscope = require("mini.indentscope")
+			return {
+				draw = {
+					animation = indentscope.gen_animation.none(),
+				},
+			}
+		end,
+	},
 }
