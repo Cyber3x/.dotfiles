b6c5c33440d4c911e88294d9c971421290ec5657
cyber3x <neven.lukic8@gmail.com>
2025-09-10 23:15:43 +0200

    fixed colorscheme, messed up tmux conf

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,25 +1,19 @@
 return {
-	-- {
-	--     -- enabled = false,
-	--     "folke/tokyonight.nvim",
-	--     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-	--     priority = 1000, -- make sure to load this before all the other start plugins
-	--     config = function()
-	--         vim.cmd([[colorscheme tokyonight]])
-	--     end,
-	-- },
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		config = function()
-			--  TODO: figure out if this even works, since it looks
-			-- way different than in the pictures
-			vim.cmd([[colorscheme catppuccin-macchiato]])
-
-			-- require("catppuccin").setup({
-			--     auto_integrations = true,
-			-- })
+		opts = {
+			flavour = "mocha",
+			integrations = {
+				lualine = true,
+				nerdtree = true,
+				cmp = true,
+			},
+		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "nightfly",
+				theme = "catppuccin",
 				component_separators = "|",
 				section_separators = "",
 			},
