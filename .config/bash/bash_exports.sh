498575ca6a10c42b068485c02f13c45064fb111e
cyber3x <neven.lukic8@gmail.com>
2026-05-04 19:05:34 +0200

    minor tweaks

--- .config/bash/bash_exports.sh
+++ .config/bash/bash_exports.sh
@@ -1,22 +1,26 @@
 # --- History settings ---
-HISTCONTROL=ignoreboth         # No duplicate lines or leading spaces
-shopt -s histappend            # Append history instead of overwrite
-HISTSIZE=1000                  # Number of commands stored in memory
-HISTFILESIZE=2000              # Number of commands stored in file
+HISTCONTROL=ignoreboth # No duplicate lines or leading spaces
+shopt -s histappend    # Append history instead of overwrite
+HISTSIZE=1000          # Number of commands stored in memory
+HISTFILESIZE=2000      # Number of commands stored in file
 
 # --- Terminal behavior ---
-shopt -s checkwinsize          # Update window size after each command
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"  # Prettify less
+shopt -s checkwinsize                                        # Update window size after each command
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)" # Prettify less
 
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
 export MANPAGER='nvim +Man!'
 
+export ANDROID_HOME="$HOME/Android/Sdk"
+
 PATH_DIRS=(
-    /opt/nvim-linux-x86_64/bin # nvim
-    "$HOME/.config/composer/vendor/bin" # php composer
-    "$HOME/.local/share/JetBrains/Toolbox/scripts" # make jetbrains tool executable from cmd
-    "$HOME/tools/imagemagick"
+  /opt/nvim-linux-x86_64/bin                     # nvim
+  "$HOME/.config/composer/vendor/bin"            # php composer
+  "$HOME/.local/share/JetBrains/Toolbox/scripts" # make jetbrains tool executable from cmd
+  "$HOME/tools/imagemagick"
+  "$ANDROID_HOME/tools"
+  "$ANDROID_HOME/platform-tools"
 )
 
 # Loop through and add if not already present
@@ -25,4 +29,3 @@ for dir in "${PATH_DIRS[@]}"; do
     export PATH="$PATH:$dir"
   fi
 done
-
