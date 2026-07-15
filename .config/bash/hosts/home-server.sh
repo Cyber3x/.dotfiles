0cb6826e655dce2841cadd67aaf3f24b7209ff7e
Cyber3x <neven.lukic8@gmail.com>
2026-07-15 23:46:05 +0200

    colors

--- .config/bash/hosts/home-server.sh
+++ .config/bash/hosts/home-server.sh
@@ -1,7 +1,2 @@
-# Overrides for the home server.
-# NOTE: the filename must match the output of `hostname` on that machine.
-# Anything can go here (aliases, exports, ...), it loads after the shared
-# files and before bash_prompt.sh.
-
-C_USER='\[\e[38;5;196;1m\]' # red
-C_PATH='\[\e[38;5;220;1m\]' # yellow
+C_HOST='\[\e[38;5;196;1m\]' # red
+#C_HOST='\[\e[38;5;220;1m\]' # yellow
