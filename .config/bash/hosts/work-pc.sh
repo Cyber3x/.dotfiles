2eed468655c1eafdddd3f3da85dbb186274e43e6
Cyber3x <neven.lukic8@gmail.com>
2026-07-16 00:05:26 +0200

    remove

--- .config/bash/hosts/work-pc.sh
+++
@@ -1,7 +0,0 @@
-# Overrides for the work PC.
-# NOTE: the filename must match the output of `hostname` on that machine.
-# Anything can go here (aliases, exports, ...), it loads after the shared
-# files and before bash_prompt.sh.
-
-C_USER='\[\e[38;5;39;1m\]'  # blue
-C_PATH='\[\e[38;5;114;1m\]' # green
