1a076d06ce38eb0b9e5ad793c0ed305ad24b14fd
cyber3x <neven.lukic8@gmail.com>
2026-08-12 14:01:48 +0200

    quit ssh on leaving session

--- .config/bash/hosts/home-server.sh
+++ .config/bash/hosts/home-server.sh
@@ -3,5 +3,5 @@ C_PATH='\[\e[38;5;228;1m\]'
 
 # start or attach to a tmux session on ssh connection
 if [[ $- == *i* ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]] && command -v tmux >/dev/null; then
-  tmux new-session -A -s server
+  exec tmux new-session -A -s server
 fi
